1e1ca75ff76a5813ea4686b160e7e9ad7ba83b90
VladofWallachia <vladb_15@yahoo.com>
2015-07-15 19:05:25 +0300

    With textures

--- in2gpu_tutorials/Chapter_2/BasicEngine/Rendering/Texture/TextureLoader.cpp
+++ in2gpu_tutorials/Chapter_2/BasicEngine/Rendering/Texture/TextureLoader.cpp
@@ -17,12 +17,12 @@ unsigned int TextureLoader::LoadTexture(const std::string& filename, unsigned in
 	unsigned char* data;
 	LoadBMPFile(filename, width, height, data);
 
-	//creeaza textura OpenGL
+	//create the OpenGL texture
 	unsigned int gl_texture_object;
 	glGenTextures(1, &gl_texture_object);
 	glBindTexture(GL_TEXTURE_2D, gl_texture_object);
 
-	//filtrare
+	//filtering
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
@@ -33,24 +33,26 @@ unsigned int TextureLoader::LoadTexture(const std::string& filename, unsigned in
 
 	//cand lucram cu texturi cu dimensiuni non multiple de 4 trebuie sa facem cititorul de randuri
 	//ce incarca texturile in OpenGL sa lucreze cu memorie aliniata la 1 (default este la 4)
+	//when we work with textures of sizes not divisible by 4 we have to use the line reader
+	//which loads the textures in OpenGL so as it can work with a 1 alligned memory (default is 4)
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-	//genereaza textura
+	//Generates texture
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 
-	//distruge array-ul din RAM
+	//eliminates the array from the RAM
 	delete data;
 
-	//creaza ierarhia de mipmapuri
+	//creates the mipmap hierarchy
 	glGenerateMipmap(GL_TEXTURE_2D);
 
-	//returneaza obiectul textura
+	//returns the texture object
 	return gl_texture_object;
 }
 
 void TextureLoader::LoadBMPFile(const std::string& filename, unsigned int& width, unsigned int& height, unsigned char*& data)
 {
-	//citim din fisier
+	//read the file
 	std::ifstream file(filename.c_str(), std::ios::in | std::ios::binary);
 	if (!file.good()){
 		std::cout << "Texture Loader: Cannot open texture file ";
@@ -60,7 +62,7 @@ void TextureLoader::LoadBMPFile(const std::string& filename, unsigned int& width
 	}
 
 
-	//citesc headere
+	//reads the headers
 	Texture::BMP_Header h; Texture::BMP_Header_Info h_info;
 	file.read((char*)&(h.type[0]), sizeof(char));
 	file.read((char*)&(h.type[1]), sizeof(char));
@@ -70,10 +72,10 @@ void TextureLoader::LoadBMPFile(const std::string& filename, unsigned int& width
 	file.read((char*)&(h.offBits), sizeof(int));
 	file.read((char*)&(h_info), sizeof(Texture::BMP_Header_Info));
 
-	//aloc memorie (un pixel are 3 componente R, G, B)
+	//assigning memory (a pixel has 3 components, R, G, B)
 	data = new unsigned char[h_info.width*h_info.height * 3];
 
-	// verific daca exista grupuri de 4 octeti pe linie
+	// check if the line contains 4 byte groups
 	long padd = 0;
 	if ((h_info.width * 3) % 4 != 0) padd = 4 - (h_info.width * 3) % 4;
 
@@ -83,12 +85,12 @@ void TextureLoader::LoadBMPFile(const std::string& filename, unsigned int& width
 
 	long pointer;
 	unsigned char r, g, b;
-	//citesc matricea
+	//reading the matrix
 	for (unsigned int i = 0; i < height; i++)
 	{
 		for (unsigned int j = 0; j < width; j++)
 		{
-			file.read((char*)&b, 1);	//in bmp ordinea in pixel este b,g,r (specific windows)
+			file.read((char*)&b, 1);	//in bmp, the component order in the pixel is b,g,r (in Windows)
 			file.read((char*)&g, 1);
 			file.read((char*)&r, 1);
 
